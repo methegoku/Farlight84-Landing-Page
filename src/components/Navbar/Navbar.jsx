3fd3a72f294c225eaca07ab8142d2e04ad737d27
Gokul Elumalai <gokulelumalai46@gmail.com>
2025-06-17 09:47:58 +0530

    Started Navbar Component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import React from "react"
+import "./Navbar.module.style.css"
+
+export default function Navbar(){
+    
+    const Navbar = () => {
+
+        const [openEsports, setOpenEsports] = useState(false);
+        const [openCreatrors, setOpenCreators] = useState(fasle);
+
+        return(
+            <nav className="navbar">
+                
+            </nav>
+        )
+    }
+}
